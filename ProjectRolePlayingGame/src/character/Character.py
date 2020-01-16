cc1eb23788ddad0c9543e5f0a78adb39dd098297
Jérôme Legriffon <jerome.legriffon@etu.unicaen.fr>
2020-01-16 23:59:58 +0100

    Ajout de plusieurs champs dans Character : age, race, bio. Méthode sérialiser.

--- ProjectRolePlayingGame/src/character/Character.py
+++ ProjectRolePlayingGame/src/character/Character.py
@@ -1,10 +1,17 @@
 class Character(object):
     ''' Character creation class
-        nom, origine, age, race, bio
     '''
-    def __init__(self, name, birthplace):
+    def __init__(self, name, birthplace, age, race, bio):
         self.name = name
         self.birthplace = birthplace
+        self.age = age
+        self.race = race
+        self.bio = bio
+
+    ''' Serialize the data in string for JSON format.
+    '''
+    def serialize(self):
+        return '{"name": "'+self.name+'", "birthplace": "'+self.birthplace+'", "age": "'+str(self.age)+'", "race": "'+self.race+'", "bio": "'+self.bio+'"}'
 
     ''' Get the name of the character
     '''
@@ -26,7 +33,37 @@ class Character(object):
     def set_birthplace(self, birthplace):
         self.birthplace = birthplace
 
+    ''' Get the age of the character
+    '''
+    def get_age(self):
+        return self.age
+
+    ''' Set the age of the character
+    '''
+    def set_age(self, age):
+        self.age = age
+
+    ''' Get the race of the character
+    '''
+    def get_race(self):
+        return self.race
+
+    ''' Set the race of the character
+    '''
+    def set_race(self, race):
+        self.race = race
+
+    ''' Get the bio of the character
+    '''
+    def get_bio(self):
+        return self.bio
+
+    ''' Set the bio of the character
+    '''
+    def set_bio(self, bio):
+        self.bio = bio
+
     ''' Represent a brief description of the character
     '''
     def __str__(self):
-        return self.name + " from " + self.birthplace
+        return self.name + "\n" + self.birthplace + "\n" + str(self.age) + "\n" + self.race + "\n" + self.bio
--- ProjectRolePlayingGame/src/main.py
+++ ProjectRolePlayingGame/src/main.py
@@ -9,7 +9,7 @@ from character.Character import *
 def personnage_creer():
     histoire = LabelFrame(fiche, text = "Information", padx = 20, pady = 20)
     histoire.pack(fill = "both", expand = "yes")
-    char = Character(nom_entry.get(), where_entry.get())
+    char = Character(nom_entry.get(), where_entry.get(), age_entry.get(), race_entry.get(), bio_entry.get(1.0, END))
 
     tfidf = TfidfVectorizer()
     raw_origine = bio_entry.get(1.0, END)
@@ -20,37 +20,15 @@ def personnage_creer():
     response = tfidf.fit_transform(phrase)
 
     Label(histoire, text = "Nom : {}".format(char.get_name())).pack()
-    Label(histoire, text = "Origine : {}".format(char.get_where())).pack()
+    Label(histoire, text = "Origine : {}".format(char.get_birthplace())).pack()
+    Label(histoire, text = "Age : {}".format(char.get_age())).pack()
+    Label(histoire, text = "Race : {}".format(char.get_race())).pack()
     
     features_names = tfidf.get_feature_names()
     for col in response.nonzero()[1]:
         Label(histoire, text = "{} - {}".format(features_names[col], response[0, col])).pack()
     # return Character, tf_idf bio
 
-def compute_TF(word_dict, bow):
-    tf_dict = {}
-    bow_count = len(bow)
-    for word, count in word_dict.items():
-        tf_dict[word] = count / float(bow_count)
-    return tf_dict
-
-def compute_IDF(doc_list):
-    idf_dict = {}
-    n = len(doc_list)
-    idf_dict = dict.fromkeys(doc_list[0].keys(), 0)
-    for doc in doc_list:
-        for word, val in doc.items():
-            if val > 0:
-                idf_dict[word] += 1
-    for word, val in idf_dict.items():
-        idf_dict[word] = math.log10(n / float(val))
-    return idf_dict
-
-def compute_TFIDF(tf_bow, idfs):
-    tfidf = {}
-    for word, val in tf_bow.items():
-        tfidf[word] = val * idfs[word]
-    return tfidf
 
 if __name__ == "__main__":
     # Programme principale
@@ -72,23 +50,43 @@ if __name__ == "__main__":
     where = StringVar()
     where.set("Bricquebec")
     where_entry = Entry(questionnaire, textvariable = where, width = 50)
+    # L'age
+    age_label = Label(questionnaire, text="Age")
+    age = StringVar()
+    age.set("29")
+    age_entry = Entry(questionnaire, textvariable = age, width = 50)
+    # La race
+    race_label = Label(questionnaire, text="Race")
+    race = StringVar()
+    race.set("Human")
+    race_entry = Entry(questionnaire, textvariable = race, width = 50)
     # La bio
     bio_label = Label(questionnaire, text = "Biographie")
     bio_entry = Text(questionnaire, width = 100)
 
-    print(nom_entry.get())
     # Boutton créer
     create_button = Button(questionnaire, text = "Créer", command=personnage_creer)
 
     nom_label.pack()
     nom_entry.pack()
+
     where_label.pack()
     where_entry.pack()
+    
+    age_label.pack()
+    age_entry.pack()
+    
+    race_label.pack()
+    race_entry.pack()
+
     bio_label.pack()
     bio_entry.pack()
     create_button.pack()
 
-    
+    # Test char & serialize
+    char = Character("Jason", "MURICA", 1776, "humanoid", "Fuck yeah")
+    print(char)
+    print(char.serialize())
 
     questionnaire.pack(side = LEFT)
     fiche.pack(side = RIGHT)
